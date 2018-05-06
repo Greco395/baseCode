53013f68a7ba8309dbf21974b9aad0d524bb70d3
Greco395 <loop@greco395.it>
2018-05-06 19:10:11 +0200

    Update class_base.php

--- class_base.php
+++ class_base.php
@@ -337,9 +337,9 @@ class ACCOUNT{
       if($user['email_token'] == $token){
           $stmt = $dbh->prepare("UPDATE ".CONFIG['DB']['USERS_TABLE']." SET status = ? WHERE email_token = ? ");
           $stmt->execute(array(1, $token));
-	      return 1;
+	  return true;
       }else{
-          return 0;
+          return false;
       }
   }
   public function getUserByName($name){
@@ -366,58 +366,4 @@ class ACCOUNT{
 }
 $set_config = new HTML;
 $set_config->bootstrapCDN(CONFIG['MISC']['USE_BOOTSTRAPcdn']);
-
-
-/*        EXAMPLE USAGE
-
-/////////// include the basecode ///////////////
-
-include("class_base.php");
-// OR
-// require_once("class_base.php");
-
-////////////////////////////////////////////////
-//////// create a new users table //////////////
-
-$config = new CONFIGURATION;
-echo $config->createUsersTable("users");
-
-///////////////////////////////////////////////
-/////////// simple login example //////////////
-
-$login = new ACCESS;
-$login->login();
-echo $login->newLogin();
-
-///////////////////////////////////////////////
-////////// simple registration example ////////
-
-$signup = new REGISTER;
-echo $signup->newMember();
-
-///////////////////////////////////////////////
-///////// simple captcha check example ////////
-
-$captcha = new CAPTCHA;
-  if($captcha->check == 1){
-     // your code here
-  }
-
-///////////////////////////////////////////////
-/////////// simple account activation /////////
-
-$activate = new ACCOUNT;
-echo $activate->ActivateAccount($token);
-
-///////////////////////////////////////////////
-///////// simple get account information //////
-
-$info = new ACCOUNT;
-echo $info->getUserByName($username)['rank'];
-echo $info->getUserByEmail($email)['username'];
-echo $info->getUserByEmailToken($token)['password'];
-
-///////////////////////////////////////////////
-
-*/
 ?>
